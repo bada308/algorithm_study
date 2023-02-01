92e533985571de6ade7dde46a77a3f63be14dac2
ps9319 <73173356+ps9319@users.noreply.github.com>
2023-02-02 03:12:53 +0900

    Update 15486.py

--- week5/15486.py
+++ week5/15486.py
@@ -2,7 +2,7 @@ N = int(input())
 
 T = [0 for _ in range(N)]
 P = [0 for _ in range(N)]
-d = [0 for _ in range(N + 1)]
+d = [0 for _ in range(N)]
 
 for i in range(N):
     T_num, P_num = map(int, input().split())
@@ -10,24 +10,23 @@ for i in range(N):
     P[i] = P_num
 
 if T[0] == 1:
-    d[1] = P[0]
+    d[0] = P[0]
 
-
-
-for i in range(2, N + 1):
+for i in range(1, N):
     tmp_list = list()
-    for j in range(i - 1, 0, -1):
+    for j in range(i, -1, -1):
         tmp_1 = j + T[j]
         tmp_2 = i
-        if j + T[j] == i:
-            tmp_list.append(P[j])
+        if j + T[j] - 1 == i:
+            tmp_list.append(P[j] + d[j-1])
     if tmp_list:
-        d[i] = d[i-1] + max(tmp_list)
+        d[i] = max(tmp_list)
     else:
         if d[i-1] != 0:
             d[i] = d[i - 1]
 
 
+
 print(d[-1])
 
-# 좀만 고치면 될 거 같은데 시간이 없어서 일단 올림....ㅠㅠㅠㅠㅠ
+# 고쳤는데 시간 초과 떠서 시간 어떻게 줄일지 찾아봐야겠다..
