47d694a57e944f7158e1823be0866105412f3a83
ps9319 <73173356+ps9319@users.noreply.github.com>
2023-02-03 16:31:47 +0900

    Add files via upload

--- week5/2579.py
+++ week5/2579.py
@@ -50,3 +50,60 @@ for i in range(n):
 print(Solution(stair_list))
 # 반례 3 5 10 9 2 1 2 5 2 9, 정답 : 37, 출력 : 33
 # 개똥같은 코드.. 첨부터 잘못 만들어버렸다..
+
+"""
+def Cal(d, stair_list):
+    for i in range(3, len(stair_list)-1):
+        # n-1칸을 안 밟았을 때
+        if d[i - 1] == d[i - 2]:
+            d[i] = d[i - 1] + stair_list[i - 1]
+        # n-1칸을 밟았을 때
+        else:
+            # n-2칸을 안 밟았을 때
+            if d[i - 2] == d[i - 3]:
+                if stair_list[i] + stair_list[i + 1] > stair_list[i - 1]:
+                    d[i] = d[i - 1]
+                else:
+                    d[i] = d[i - 1] + stair_list[i - 1]
+            # n-2칸을 밟았을 때
+            else:
+                d[i] = d[i - 1]
+
+    return d
+
+
+def Solution(stair_list):
+    stair_list.reverse()
+    stair_list.append(0)
+    stair_list.append(0)
+
+    if len(stair_list) < 3:
+        return sum(stair_list)
+    else:
+        # 경우의 수 두 개
+        # OOX일 때
+        d = [0 for _ in range(len(stair_list) + 1)]
+        d[0] = 0
+        d[1] = stair_list[0]
+        d[2] = stair_list[0] + stair_list[1]
+        result_1 = Cal(d, stair_list)[-3]
+        # OXO일 때
+        d = [0 for _ in range(len(stair_list) + 1)]
+        d[0] = 0
+        d[1] = stair_list[0]
+        d[2] = stair_list[0]
+        result_2 = Cal(d, stair_list)[-3]
+
+    return max(result_1, result_2)
+
+n = int(input())
+stair_list = list()
+for i in range(n):
+    stair = int(input())
+    stair_list.append(stair)
+
+
+print(Solution(stair_list))
+#고치다 망한 코드 2
+#내 코드 살리면서 바꿔보려했는데 그냥 싸그리 엎고 새로 짜야겠다..
+"""
